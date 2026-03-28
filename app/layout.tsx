9fd4b6bbf62aebc8de5aedea4a2e733fa2a63594
Mario Russo <mariorusso@gmail.com>
2026-03-28 20:13:51 -0300

    fix: set global runtime to edge

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
+export const runtime = "edge";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
